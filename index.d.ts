0b008bf1b5beefe3e60ad53082da7adbefe3efe1
Jinxin Hu <hjxenjoy@gmail.com>
2021-03-16 16:00:08 +0800

    docs: update d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 export function array2bool(array: string[]): Record<string, boolean>
 export function array2bool(array: number[]): Record<number, boolean>
 export function array2bool<T extends { [x: string]: any }>(array: T[], primaryKey: keyof T): Record<string, boolean>
-
 export function array2item<T extends { [x: string]: any }>(array: T[], primaryKey: keyof T): Record<string, T>
